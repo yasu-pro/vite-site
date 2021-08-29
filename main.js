d0d0cfb2a5d215e7e5c5658bc55f4a023b67b9f9
yasu-pro <cb1-y.s@docomo.ne.jp>
2021-08-29 23:10:43 +0900

    feat add sum function in main.js and DOM in index.html

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
-import './style.css'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
+document.querySelector("#app").innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+`;
+const number1 = document.getElementById("js-number1");
+const number2 = document.getElementById("js-number2");
+const total = document.getElementById("js-total");
+const btn = document.getElementById("js-btn");
+
+function sum(a, b) {
+  const result = a + b;
+  return result;
+}
+
+btn.addEventListener(click, () => {
+  const sumResult = sum(number1, number2);
+  total.textContent = sumResult;
+});
